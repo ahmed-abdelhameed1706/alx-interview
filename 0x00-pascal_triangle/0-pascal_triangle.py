58c0a7d6a28e75eed0c4406551aa36326f0b29f4
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-04 19:51:56 +0200

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     t = [[1]]
-    for i in range (1, n):
+    for i in range(1, n):
         t.append([1])
         for j in range(1, i):
             t[i].append(t[i-1][j-1] + t[i-1][j])
