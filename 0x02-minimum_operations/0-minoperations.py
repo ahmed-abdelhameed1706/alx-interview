07a96529577f4f916b8b228e22bcb3b55abeb431
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-17 02:58:34 +0200

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,36 +3,37 @@
 import math
 
 
-def gcd(n):
-    """function to find the greatest number to divide n by"""
-    result = 1
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            result = max(result, i, n // i)
-    return result
-
-
-def is_prime(number):
-    """function to find if number is prime"""
-    if number < 2:
-        return False
-    for i in range(2, int(math.sqrt(number)) + 1):
-        if number % i == 0:
-            return False
-    return True
-
-
 def minOperations(n: int) -> int:
-    """function to find minimum operations"""
-    if n <= 0:
-        return 0
+    def gcd(n):
+        """function to find the greatest number to divide n by"""
+        result = 1
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                result = max(result, i, n // i)
+        return result
 
-    if is_prime(n) or n == 1:
-        return n
+    def is_prime(number):
+        """function to find if number is prime"""
+        if number < 2:
+            return False
+        for i in range(2, int(math.sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+        return True
+        """function to find minimum operations"""
 
-    gcd_n = gcd(n)
-    div_factor = n // gcd_n
+    def rec_min(x):
+        if x <= 0:
+            return 0
 
-    if is_prime(gcd_n) or gcd_n == 1:
-        return gcd_n + div_factor
-    return minOperations(gcd_n) + div_factor
+        if is_prime(x) or n == 1:
+            return x
+
+        gcd_n = gcd(x)
+        div_factor = x // gcd_n
+
+        if is_prime(gcd_n) or gcd_n == 1:
+            return gcd_n + div_factor
+        return minOperations(gcd_n) + div_factor
+
+    return rec_min(n)
