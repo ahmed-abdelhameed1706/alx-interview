ca7a2c8cff48fd9570bfdd00cf1116dfe82bf3c7
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-17 03:01:56 +0200

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,15 +4,17 @@ import math
 
 
 def minOperations(n: int) -> int:
-    def gcd(n) -> int:
+    """function to find minimum operations"""
+
+    def gcd(n: int) -> int:
         """function to find the greatest number to divide n by"""
-        result = 1
+        result: int = 1
         for i in range(2, int(n**0.5) + 1):
             if n % i == 0:
                 result = max(result, i, n // i)
         return result
 
-    def is_prime(number) -> bool:
+    def is_prime(number: int) -> bool:
         """function to find if number is prime"""
         if number < 2:
             return False
@@ -20,9 +22,9 @@ def minOperations(n: int) -> int:
             if number % i == 0:
                 return False
         return True
-        """function to find minimum operations"""
 
-    def rec_min(x) -> int:
+    def rec_min(x: int) -> int:
+        """function to recursivly find the operations"""
         if x <= 0:
             return 0
 
@@ -30,7 +32,7 @@ def minOperations(n: int) -> int:
             return x
 
         gcd_n = gcd(x)
-        div_factor = x // gcd_n
+        div_factor: int = x // gcd_n
 
         if is_prime(gcd_n) or gcd_n == 1:
             return gcd_n + div_factor
