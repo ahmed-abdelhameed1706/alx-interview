991e127deba4f70a57af16b05387d7acf0b657a6
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-25 09:07:25 +0200

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         r'"GET /projects/260 HTTP/1\.1" \d+ \d+$'
     )
 
-    def validate_line(line):
+    def validate_line(line: str) -> bool:
         """function to return line matches format or not"""
         patern = re.compile(line_format)
         return bool(patern.match(line))
@@ -27,24 +27,23 @@ if __name__ == "__main__":
         500: 0,
     }  # no
     total_size = 0
-    line_count = 0
 
-    def print_data():
+    def print_data() -> None:
+        """function to print the data"""
         print(f"File size: {total_size}")
         for key in sorted(status_code_count.keys()):
             if status_code_count[key] > 0:
                 print(f"{key}: {status_code_count[key]}")
 
     try:
-        for line in sys.stdin:
+        for idx, line in enumerate(sys.stdin):
             if validate_line(line):
-                line_count += 1
                 file_size = line.split()[-1]
                 status_code = int(line.split()[-2])
                 total_size += int(file_size)
                 if status_code in status_code_count.keys():
                     status_code_count[status_code] += 1
-                if line_count % 10 == 0:
+                if (idx + 1) % 10 == 0:
                     print_data()
     finally:
         print_data()
