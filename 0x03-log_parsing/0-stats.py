dded0ab54b307f981abab0bed12a6214641cc84b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-01-25 10:00:59 +0200

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         500: 0,
     }  # no
     total_size = 0
+    line_count = 0
 
     def print_data() -> None:
         """function to print the data"""
@@ -32,15 +33,16 @@ if __name__ == "__main__":
                 print(f"{key}: {status_code_count[key]}")
 
     try:
-        for idx, line in enumerate(sys.stdin):
+        for line in sys.stdin:
             try:
                 if validate_line(line):
+                    line_count += 1
                     file_size = line.split()[-1]
                     status_code = int(line.split()[-2])
                     total_size += int(file_size)
                     if status_code in status_code_count.keys():
                         status_code_count[status_code] += 1
-                    if (idx + 1) % 10 == 0:
+                    if line_count % 10 == 0:
                         print_data()
             except Exception as e:
                 continue
