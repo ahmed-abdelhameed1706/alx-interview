49c3dcdeae2a4dec138f5d5086b31c862fc9e651
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2024-03-01 03:33:53 +0200

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,7 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    memo = [total + 1] * (total + 1)
+    memo = [float("inf")] * (total + 1)
     memo[0] = 0
 
     for i in range(1, total + 1):
@@ -15,4 +15,4 @@ def makeChange(coins, total):
             if i - j >= 0:
                 memo[i] = min(memo[i - j] + 1, memo[i])
 
-    return memo[total] if memo[total] != total + 1 else -1
+    return memo[total] if memo[total] != float("inf") else -1
